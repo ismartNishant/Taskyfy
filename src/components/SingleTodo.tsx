485df4edc288fd911e02bd087cb1621083b84246
ismartNishant <nishantrajputall@gmail.com>
2023-02-16 11:04:07 +0530

    simpl todo

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState , useRef , useEffect} from 'react';
 import { Todo } from '../model';
 import Form from 'react-bootstrap/Form';
 
@@ -12,7 +12,15 @@ type Props = {
 
 const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
   const [edit, setEdit] = useState<Boolean>(false);
-  const [editTodo, setEditTodo] = useState(todo.todo);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => todo.id === id ? { ...todo, todo: editTodo } : todo)
+    );
+    setEdit(false);
+  }
 
   const handleDone = (id: number) => {
     setTodos(
@@ -26,12 +34,17 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   }
 
- 
+const inputRef = useRef<HTMLInputElement>(null);
+useEffect(() => {
+   inputRef.current?.focus();
+}, [edit])
+
   return (
 
-    <Form className='single-todos d-flex justify-content-between align-items-center'>
+    <Form className='single-todos d-flex justify-content-between align-items-center' onSubmit={(e) => handleEdit(e, todo.id)} >
       {edit ? (
-        <Form.Control type="text"  />
+        <input ref={inputRef} value={editTodo} onChange={(e) => setEditTodo(e.target.value)} className="editable-todo-text" />
+
       ) :
         todo.isDone ? (
           <s className='single-todos-text'>{todo.todo}</s>
@@ -53,7 +66,7 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
         <span><i className='bx bx-check bx-tada-hover border-light border p-1 rounded-circle' onClick={() => handleDone(todo.id)} ></i></span>
       </div>
     </Form>
-    
+
 
   )
 }
